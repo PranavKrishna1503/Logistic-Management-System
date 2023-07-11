52b1b41f271167baa681e3045e0d880e466c7d55
PranavKrishna1503 <727721euai042@skcet.ac.in>
2023-07-11 11:27:31 +0530

    Revert "Add files via upload"
    This reverts commit 24dcc293c88165c5124062c2a38f3487d4a9877c.

--- homepage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function Home(){
-    return(
-        <p>This is homepage</p>
-    );
-}
--- redux.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-const reducer= (state={counter: 0},action) =>{
-
-}
-const store=createStore(reducer);
-export default store;
