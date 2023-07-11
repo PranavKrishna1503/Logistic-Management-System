24dcc293c88165c5124062c2a38f3487d4a9877c
PranavKrishna1503 <113581038+PranavKrishna1503@users.noreply.github.com>
2023-07-11 09:43:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import Login from './login';
+import './styles.css';
+
+function App() {
+  return (
+<Login/>
+  
+  );
+}
+
+export default App;
---
+++ homepage.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Home(){
+    return(
+        <p>This is homepage</p>
+    );
+}
